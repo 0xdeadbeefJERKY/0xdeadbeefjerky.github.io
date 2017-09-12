7bba6ed4cda6504bb18d18f76d089f5f50a85891
Dominic <dominic.spinosa@gmail.com>
2017-09-12 00:45:03 -0400

    fixed Medium crossposting

--- _plugins/jekyll-crosspost-to-medium.rb
+++ _plugins/jekyll-crosspost-to-medium.rb
@@ -166,8 +166,7 @@ module Jekyll
           'contentFormat' => "html",
           'content'       => content,
           'tags'          => tags,
-          #'publishStatus' => @settings['status'] || "public",
-          'publishStatus' => @settings['status'] || "draft",
+          'publishStatus' => @settings['status'] || "public",
           'publishedAt'   => published_at.iso8601,
           'license'       => @settings['license'] || "all-rights-reserved",
           'canonicalUrl'  => canonical_url
